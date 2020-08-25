66d3df4f8e918252343a5b65ddaa67d9466fc247
batboy118 <118jjh@naver.com>
2020-08-25 16:15:16 +0900

    카카오2018_3차

---
+++ 4.Programmers/17684_2018카카오.js
@@ -0,0 +1,29 @@
+// Programmers
+// no. 17684
+// [3차] 압축
+// https://programmers.co.kr/learn/courses/30/lessons/17684
+// by JH (batboy118)
+
+function solution(msg) {
+    let answer = [];
+    let map = {};
+    let index = 1;
+    for(let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++){
+        const c = String.fromCharCode(i);
+        if(map[c] === undefined){
+            map[c] = index++;
+        }
+    }
+    for(let i = 0 ; i < msg.length; i++){
+        let str = msg[i];
+        let val;
+        while(map[str] && i < msg.length){
+            val = map[str];
+            str += msg[++i];
+        }
+        answer.push(val);
+        map[str] = index++;
+        i--;
+    }
+    return answer;
+}
---
+++ 4.Programmers/17685_2018카카오.js
@@ -0,0 +1,30 @@
+// Programmers
+// no. 17685
+// [3차] 자동완성
+// https://programmers.co.kr/learn/courses/30/lessons/17685
+// by JH (batboy118)
+
+function solution(words) {
+    var answer = 0;
+    words = words.sort();
+    for(let i = 0; i < words.length; i++){
+        let a = 0,b = 0;
+        for(let j = 0; i > 0 && j < words[i].length ; j++){
+            if((words[i][j] !== words[i - 1][j])){
+                a = (j + 1);
+                break;
+            }
+            if(j === words[i].length - 1) a = words[i].length;
+        }
+        for(let j = 0; i < words.length - 1 && j < words[i].length ; j++){
+            if((words[i][j] !== words[i + 1][j])){
+                b = (j + 1);
+                break;
+            }
+            if(j === words[i].length - 1) b = words[i].length;
+        }
+        if(a > b) answer += a;
+        else answer += b;
+    }
+    return answer;
+}
