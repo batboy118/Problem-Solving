5b7e9ba2d9e6ea7b9d27f4b1c8e1e14523e344f7
Junhyeong Jo <118jjh@naver.com>
2020-10-12 23:58:26 +0900

    boj 19237 not done

---
+++ 1.BOJ/19237.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string.h>
+
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+#define PRINT_ARR(n, target) for(int i = 0; i < n; i++){ \
+								cout << target << ' '; \
+							} \
+							cout <<'\n';\
+
+#define PRINT_ARR2(n,m, target)	for(int i = 0; i < n; i++){ \
+								for(int j = 0; j < m; j++){ \
+									cout << target << ' '; \
+								} \
+								cout <<'\n';\
+							}
+
+using namespace std;
+
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { -1, 1, 0, 0 };
+int cnt;
+
+
+
+struct Shark {
+	int y, x;
+	int is_dead = 0;
+	int dir;
+	int p_dir[4][4];
+};
+
+struct Land {
+	int shark = 0;
+	int smell = 0;
+	int time = 0;
+};
+
+int N, M, k;
+int r;
+Land board[20][20];
+Shark sharks[404];
+
+int is_edge(int y, int x) {
+	return y < 0 || x < 0 || y >= N || x >= N;
+}
+
+void move() {
+	for (int i = 1; i <= M; i++) {
+		Shark& shark = sharks[i];
+		if (shark.is_dead) continue;
+		int cx = shark.x, cy = shark.y;
+		int dir = shark.dir;
+		int ny, nx;
+		int found = 0;
+		board[cy][cx].shark = 0;
+		for (int j = 0; j < 4; j++) {
+			int d = shark.p_dir[dir][j];
+			ny = cy + dy[d];
+			nx = cx + dx[d];
+			if(is_edge(ny, nx)) continue;
+			if (board[ny][nx].time == 0 || cnt - board[ny][nx].time > k) { //
+				found = 1;
+				if (board[ny][nx].shark > 0 && board[ny][nx].shark < i) {
+					shark.is_dead = 1;
+					r--;
+					break;
+				}
+				board[ny][nx].shark = i;
+				shark.y = ny;
+				shark.x = nx;
+				shark.dir = d;
+				break;
+			}
+		}
+		if (found) continue;
+		for (int j = 0; j < 4; j++) {
+			int d = shark.p_dir[dir][j];
+			ny = cy + dy[d];
+			nx = cx + dx[d];
+			if (is_edge(ny, nx)) continue;
+			if (board[ny][nx].smell == i) {
+				found = 1;
+				if (board[ny][nx].shark > 0 && board[ny][nx].shark < i) {
+					shark.is_dead = 1;
+					r--;
+					break;
+				}
+				board[ny][nx].shark = i;
+				shark.y = ny;
+				shark.x = nx;
+				shark.dir = d;
+				break;
+			}
+		}
+	}
+	for (int i = 1; i <= M; i++) {
+		if (sharks[i].is_dead) continue;
+		Shark& shark = sharks[i];
+		board[shark.y][shark.x].time = cnt;
+		board[shark.y][shark.x].smell = i;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N >> M >> k;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> board[i][j].shark;
+			if (board[i][j].shark) {
+				sharks[board[i][j].shark].y = i;
+				sharks[board[i][j].shark].x = j;
+				board[i][j].smell = board[i][j].shark;
+				board[i][j].time = 1;
+			}
+		}
+	}
+	r = M;
+	for (int i = 1; i <= M; i++) {
+		cin >> sharks[i].dir;
+		sharks[i].dir--;
+	}
+
+	for (int i = 1; i <= M; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+				cin >> sharks[i].p_dir[j][k];
+				sharks[i].p_dir[j][k]--;
+			}
+		}
+	}
+	cnt = 1;
+	//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+	//PRINT_ARR2(N,N, board[i][j].shark)
+	while (r != 1 && cnt <= 1000) {
+		cnt++;
+		move();
+		//cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+		//PRINT_ARR2(N, N, board[i][j].shark)
+	}
+	if (cnt > 1000) cout << -1;
+	else cout << --cnt;
+	return 0;
+}
--- 1.BOJ/19238_new.cpp
+++ 1.BOJ/19238_new.cpp
@@ -18,27 +18,113 @@ using namespace std;
 int N, M, fuel;
 int dx[4] = { 0,1,-1,0 };
 int dy[4] = { 1,0,0,-1 };
-int ty, tx;
-struct P {
+
+int ty, tx; // 택시 현재 위치
+
+struct P {  // 위치 정보( 0: 도로, 1: 벽, 2: 승객 / 승객일 경우 dest(y,x) 값 존재)
 	int state = 0;
 	pair<int, int> dest;
 };
+
 P tempP;
-vector < vector <P> > board(20, vector <P> (20, tempP)) ;
+vector < vector <P> > board(22, vector <P>(22, tempP));  //맵
+
 int visit[20][20];
+
+int isEdge(int y, int x) {
+	return y < 0 || x < 0 || y >= N || x >= N || board[y][x].state == WALL;
+}
 
 int findPerson() {
 	queue<pair<int, int>> q;
-	q.push({ty, tx});
-	while (!q.empty()) {
-		auto cur = q.front();
-		q.pop();
-
+	q.push({ ty, tx });
+	if (board[ty][tx].state == PERSON) {   //현재위치에 승객이 있으면 승객을 지우고 바로 리턴
+		board[ty][tx].state = EMPTY;
+		return 0;
 	}
+	int fy = 100, fx = 100;    // 찾은 승객의 좌표
+	int found = 0;			// 승객 찾았는지 확인 플래그
+	int dist = 0;			// 승객까지의 거리
+	while (!q.empty()) {
+		dist++;
+		int size = q.size();
+		while (size--) {
+			auto cur = q.front();
+			q.pop();
+			int y = cur.first;
+			int x = cur.second;
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+				if (isEdge(ny, nx) || visit[ny][nx]) continue;
+				if (board[ny][nx].state == PERSON) {
+					found = 1;
+					visit[ny][nx] = 1;
+					if (ny < fy) {
+						fy = ny;
+						fx = nx;;
+					}
+					else if (ny == fy) {
+						if (nx < fx) {
+							fy = ny;
+							fx = nx;
+						}
+					}
+				}
+				else if (board[ny][nx].state == EMPTY) {
+					q.push({ ny, nx });
+					visit[ny][nx] = 1;
+				}
+			}
+		}
+		if (found) break;
+	}
+	if (found == 0) return 1;
+	tx = fx, ty = fy;  // 택시위치 갱신
+	board[ty][tx].state = EMPTY;  // 승객 지우기
+	fuel -= dist;
+	return 0;
 }
 
-int toHome() {
-
+int toHome() {  //승객 찾는 로직이랑 거의 유사
+	queue<pair<int, int>> q;
+	q.push({ ty, tx });
+	if (board[ty][tx].dest.first == ty && board[ty][tx].dest.second == tx) {  // 현재위치와 집의 좌표가 같으면 바로 리턴
+		return 0;
+	}
+	int targetY = board[ty][tx].dest.first;   //승객의 집 좌표
+	int targetX = board[ty][tx].dest.second;
+	int found = 0;
+	int dist = 0;
+	while (!q.empty()) {
+		dist++;
+		int size = q.size();
+		while (size--) {
+			auto cur = q.front();
+			q.pop();
+			int y = cur.first;
+			int x = cur.second;
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+				if (isEdge(ny, nx) || visit[ny][nx]) continue;
+				if (ny == targetY && nx == targetX) {  // 집 찾으면 끝내기
+					found = 1;
+					break;
+				}
+				else{
+					q.push({ ny, nx });
+					visit[ny][nx] = 1;
+				}
+			}
+			if (found) break;
+		}
+		if (found) break;
+	}
+	if (found == 0) return 1;
+	tx = targetX, ty = targetY;
+	fuel -= dist;
+	return 0;
 }
 
 int main() {
@@ -50,29 +136,43 @@ int main() {
 		}
 	}
 	cin >> ty >> tx;
-	ty--; tx--;
+	ty--; tx--;  // 인덱스 0번부터 시작
 	int tempX, tempY;
 	for (int i = 0; i < M; i++) {
 		cin >> tempY >> tempX;
-		tempY--; tempX--;
-		cin >> board[tempY][tempX].dest.first;
-		cin >> board[tempY][tempX].dest.second;
+		tempY--; tempX--; // 인덱스 0번부터 시작
+		board[tempY][tempX].state = 2;
+		cin >> board[tempY][tempX].dest.first >> board[tempY][tempX].dest.second;
+		board[tempY][tempX].dest.first--; // 인덱스 0번부터 시작
+		board[tempY][tempX].dest.second--; // 인덱스 0번부터 시작
 	}
 
 
-	PRINT_ARR(N, N, board[i][j].state)   //
-	PRINT_ARR(N, N, '(' << board[i][j].dest.first << ',' << board[i][j].dest.first << ')')   //
+	//PRINT_ARR(N, N, board[i][j].state)
+	//PRINT_ARR(N, N, '(' << board[i][j].dest.first << ',' << board[i][j].dest.second << ')')
 
 	int flag = 0;
 	int cnt = M;
-	while (fuel > 0 || flag == 0 || cnt) {
+
+	// cout << "-------------------" << endl;
+	while (fuel >= 0 && cnt) {
 		cnt--;
 		memset(visit, 0, sizeof(visit));
+		// cout << "출발위치 :" << ty << "," << tx << endl;
 		flag = findPerson();
-		if (flag) break;
+		//cout << "승객위치 :" << ty << "," << tx << endl;
+		//cout << "승객위치에서 남은 연료 :" << fuel << endl;
+		if (flag || fuel < 0) break;
 		memset(visit, 0, sizeof(visit));
-		toHome();
+		int startFuel = fuel;
+		flag = toHome();
+		//cout << "도착지 :" << ty << "," << tx << endl;
+		//cout << "도착지에서 남은 연료 :" << fuel << endl;
+		if (flag || fuel < 0) break;
+		fuel += (startFuel - fuel) * 2;
+		//cout << "충전 후 연료 :" << fuel << endl;
+		//cout << "-------------------" << endl;
 	}
-	if (fuel < 0 || cnt) cout << -1;
+	if (fuel < 0 || flag) cout << -1;
 	else cout << fuel;
 }
